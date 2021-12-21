e8b983337138cf6e6e40e1457e7015fc7906e82c
Nicholas Teinert <nteinert2005@gmail.com>
2021-12-21 13:16:40 -0600

    fix(auth): login issue

--- pages/api/newlogin.js
+++ pages/api/newlogin.js
@@ -1,20 +1,22 @@
 import connectDB from "../../middleware/connectDB";
-import User from '../../models/User';
+import User from "../../models/User";
 
 const handler = (req, res) => {
-    console.log('api/login reached');
+  console.log("api/login reached");
 
-    User.findOne({
-        username: req.body.user.username,
-        password: req.body.user.password
-    }, (err, user) => {
-
-        //console.log(user);
-        if(err) res.send(500);
-        if(user) {
-            res.send(user);
-        }
-    });
-}
+  User.findOne(
+    {
+      username: req.body.user.username,
+      password: req.body.user.password,
+    },
+    (err, user) => {
+      //console.log(user);
+      if (err) res.send(res.data);
+      if (user) {
+        res.send(user);
+      }
+    }
+  );
+};
 
 export default connectDB(handler);
