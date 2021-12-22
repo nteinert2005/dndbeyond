26ccdefb8e079c91b78fafe4a31e586390426cd8
Nicholas Teinert <nteinert2005@gmail.com>
2021-12-22 11:21:13 -0600

    fix(publisher): add title to page

--- pages/publishers.js
+++ pages/publishers.js
@@ -1,10 +1,14 @@
 import Hero from "./components/Hero";
 import PublisherHero from "./components/PublisherHero";
 import HomeNav from "./components/HomeNav";
+import Head from "next/head";
 
 const Publishers = () => {
   return (
     <>
+      <Head>
+        <title> Publishers - Epic Table TTRPG </title>
+      </Head>
       <HomeNav />
       <PublisherHero />
     </>
